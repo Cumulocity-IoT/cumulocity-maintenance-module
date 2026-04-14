b5e4f20d751ff4ed10cfb37a16d41ac76b066136
apes <alexander.pester@cumulocity.com>
2026-04-14 13:08:54 +0200

    fix(model): add validation annotations to MaintenancePlanListResponse fields

--- src/main/java/cumulocity/microservice/maintenancemodule/model/MaintenancePlanListResponse.java
+++ src/main/java/cumulocity/microservice/maintenancemodule/model/MaintenancePlanListResponse.java
@@ -3,9 +3,12 @@ package cumulocity.microservice.maintenancemodule.model;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.validation.annotation.Validated;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -18,13 +21,17 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Schema(description = "Response containing a list of maintenance plans")
+@Validated
 public class MaintenancePlanListResponse {
 
     @Schema(description = "List of maintenance plans")
     @JsonProperty("maintenancePlans")
+    @NotNull
+    @Valid
     private List<MaintenancePlan> maintenancePlans = new ArrayList<>();
 
     @Schema(description = "Current page number (0-based)", example = "0")
+    @NotNull
     private Integer currentPage;
 
     @Schema(requiredMode = Schema.RequiredMode.REQUIRED, description = "Number of items per page", example = "10")
@@ -32,8 +39,10 @@ public class MaintenancePlanListResponse {
     private Integer pageSize;
 
     @Schema(description = "Total number of pages", example = "5")
+    @NotNull
     private Integer totalPages;
 
     @Schema(description = "Total number of elements across all pages", example = "42")
+    @NotNull
     private Long totalElements;
 }
